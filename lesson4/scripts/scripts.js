16449afa6fd93c53f834fff1dcf9cc7c8bdb6d8b
ctfuller98 <ful17005@byui.edu>
2021-01-29 17:46:40 -0700

    updated date

--- lesson4/scripts/scripts.js
+++ lesson4/scripts/scripts.js
@@ -3,4 +3,6 @@ window.addEventListener('load', () =>{
     const mainnav = document.querySelector('#navigation');
     hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')},false);
     window.onresize = ()=>{if(window.innerWidth>760) mainnav.classList.remove('responsive');};
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toDateString();
 });
