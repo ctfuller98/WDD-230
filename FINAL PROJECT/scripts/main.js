6f5e19df57394b13899e149af4088c42c31a5b7a
ctfuller98 <ful17005@byui.edu>
2021-04-06 20:42:34 -0600

    finsihed project

--- FINAL PROJECT/scripts/main.js
+++ FINAL PROJECT/scripts/main.js
@@ -1,26 +1,29 @@
-window.addEventListener('load', () =>{
-  openNav()
-  closeNav()
+window.addEventListener('load', () => {
   showSlides()
 });
-/*-------------RESPONSIVE MENU------------------*/
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-  }
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
-  var slideIndex = 0;
-  showSlides();
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); 
-  }
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+      slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+function open_nav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
